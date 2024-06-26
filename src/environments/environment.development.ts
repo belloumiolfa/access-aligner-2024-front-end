512c06e3bae9d992afc84fabbd1fe014f3b26b00
Ferjania Gabsi <ferjeniag]gmail.com>
2024-06-26 09:58:11 +0200

    pull main

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   companyName: "Access Aligner",
   domainName: "",
-  apiBaseUrl: 'http://192.168.0.4:8080'
+  apiBaseUrl: 'http://192.168.0.6:8080'
 
   
 };
