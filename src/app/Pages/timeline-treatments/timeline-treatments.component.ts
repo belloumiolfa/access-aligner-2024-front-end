3925e2bc822d46e8816652c02c84f9142ee72fed
Ferjania Gabsi <ferjeniag]gmail.com>
2024-07-01 16:52:20 +0200

    updates commit

--- src/app/Pages/timeline-treatments/timeline-treatments.component.ts
+++ src/app/Pages/timeline-treatments/timeline-treatments.component.ts
@@ -4,24 +4,34 @@ import { PatientTreatInformationsComponent } from '../../Components/patient-trea
 import { PatientTreatTeethInformationsComponent } from '../../Components/patient-treat-teeth-informations/patient-treat-teeth-informations.component';
 import { PatientTreatPhotosInformationsComponent } from '../../Components/patient-treat-photos-informations/patient-treat-photos-informations.component';
 import { PatientTreatClinicsInformationsComponent } from '../../Components/patient-treat-clinics-informations/patient-treat-clinics-informations.component';
+import { Router, RouterModule } from '@angular/router';
+import { AppService } from '../../Core/Services/app.service';
 
 @Component({
   selector: 'app-timeline-treatments',
   standalone: true,
-  imports: [PatientDisplayInformationsComponent, PatientTreatInformationsComponent, 
-    PatientTreatTeethInformationsComponent, PatientTreatPhotosInformationsComponent,
-  PatientTreatClinicsInformationsComponent
+  imports: [
+    PatientDisplayInformationsComponent,
+    PatientTreatInformationsComponent,
+    PatientTreatTeethInformationsComponent,
+    PatientTreatPhotosInformationsComponent,
+    PatientTreatClinicsInformationsComponent,
+    RouterModule,
   ],
   templateUrl: './timeline-treatments.component.html',
-  styleUrl: './timeline-treatments.component.css'
+  styleUrl: './timeline-treatments.component.css',
 })
-export class TimelineTreatmentsComponent implements OnInit{
+export class TimelineTreatmentsComponent implements OnInit {
+  patient$!: any;
 
+  id: any;
 
-
-  constructor () {}
+  constructor(private appService: AppService, router:Router) {
+    this.appService.getPatient$.subscribe((data: any ) => {
+    this.id=data.id ; 
+    });
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-
 }
